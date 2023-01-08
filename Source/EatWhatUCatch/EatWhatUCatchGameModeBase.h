610208711ec55e7c4c111f1e92db834008953259
Pia Probst <pia.probst@online.de>
2023-01-08 13:12:23 +0100

    initial commit

---
+++ Source/EatWhatUCatch/EatWhatUCatchGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EatWhatUCatchGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EATWHATUCATCH_API AEatWhatUCatchGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
